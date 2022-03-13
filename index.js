9309288bc7e48e7db23045586ef98aecd0b236d9
daniel2992 <83758148+daniel2992@users.noreply.github.com>
2022-03-13 15:41:46 +0300

    note

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 app.listen(8080);
 
 app.get('/', function(req, res) {
-  res.send('Hello, world! bitches');  
+  res.send('Hello, world! bitches first things first i fuck');  
 })
